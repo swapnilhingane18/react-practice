83de039e6e32cf0b14c12af202bbb620d1d6881a
swapnilhingane18 <swapnilhingane9290@gmail.com>
2026-01-13 09:51:51 +0530

    Added delete function

--- src/mini-projects/ToDoList.jsx
+++ src/mini-projects/ToDoList.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default function ToDoList() {
-  const [todos, setTodos] = useState([
-    "Sample Task 1",
-    "Sample Task 2",
-  ]);
+  const [todos, setTodos] = useState([{task:"sample-task",id:uuidv4()}]);
   const [newTodo, setNewTodo] = useState("");
 
-  const addNewTask = () => {
-    if (newTodo.trim() === "") return;
+ let addNewTask = () => {
+  setTodos((prevTodos) => {
+    return [...prevTodos,{task: newTodo,id: uuidv4()}]
+  });
+  setNewTodo("");
+};
 
-    setTodos([...todos, newTodo]);
-    setNewTodo("");
-  };
+let updateToDoValue = (event)=>{
+  setNewTodo(event.target.value);
+}
 
+let deleteToDo = (id) => {
+ setTodos((prevTodos) => 
+    prevTodos.filter((todo) => todo.id !== id)
+);
+};
+  
   return (
     <div className="todo-container">
       <h2>Todo App</h2>
@@ -26,12 +35,18 @@ export default function ToDoList() {
       <button onClick={addNewTask}>Add</button>
       </div>
 
-
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+
+          <li key={todo.id}>
+           <span>{todo.task}</span>
+           &nbsp;&nbsp;
+           <button onClick={() => deleteToDo(todo.id)}>Delete</button>
+            </li>
+
         ))}
       </ul>
     </div>
   );
 }
+ 
