5db63cd32df62fd6bf891eeb25defd1a788f2e8e
swapnilhingane18 <swapnilhingane9290@gmail.com>
2026-01-15 23:24:42 +0530

    added iswinner and refresh with button

--- src/mini-projects/Lottery.jsx
+++ src/mini-projects/Lottery.jsx
@@ -8,9 +8,22 @@ function generateTicket() {
     ];
 }
 
+function sum(arr) {
+    return arr.reduce((sum,val) => sum + val, 0);
+} 
+
+generateTicket = () =>{
+    return [
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10)
+    ]
+}
+
 
 export default function Lottery() {
     let [ticket, setticket] = useState(generateTicket());
+    let iswinner = sum(ticket) == 15;
     return (
     <div>
         <h1>Lottery Game</h1>
@@ -20,6 +33,9 @@ export default function Lottery() {
             <span>{ticket[1]}</span>
             <span>{ticket[2]}</span>
         </div>
+        
+        <button onClick={() => setticket(generateTicket())}>New Ticket</button>
+        <h3>{iswinner ? "You Win!" : "Try Again"}</h3>
 
         </div>);
 }
